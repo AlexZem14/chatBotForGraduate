506e3847785d064d43f4e0316d7df168f6ceaf7f
alexzem <alex.z14@bk.ru>
2020-06-10 16:53:09 +0400

    Добавлены комментарии к функциям в файле points.py

--- vstu-abit-bot/vk_bot/points.py
+++ vstu-abit-bot/vk_bot/points.py
@@ -1,11 +1,18 @@
 def sumEgePoints(vk_id, connect):
+    """
+    Подсчет баллов ЕГЭ пользователя
+    :param vk_id: идентификатор пользователя в вк
+    :param connect: соединение с БД
+    """
     sumP = 0
+    # Подготавливаем SQL запросы для передачи их базе данных
     bdSelectExam1 = """select * from exam where id_user = %s and name = \"Математика\";"""
     bdSelectExam2 = """select * from exam where id_user = %s and name = \"Физика\";"""
     bdSelectExam3 = """select * from exam where id_user = %s and name = \"Русский\";"""
     bdSelectUser = """select * from user where id = %s;"""
     updateUserPoints = """update user set ege_points = %s where id = %s;"""
 
+    # Передаем запросы на добавление элемента и фиксируем в БД
     try:
         with connect.cursor() as cursor:
             cursor.execute(bdSelectExam1, str(vk_id))
@@ -18,18 +25,28 @@ def sumEgePoints(vk_id, connect):
             sumP = sumP + cursor.fetchall()[0]["ege_points"]
             cursor.execute(updateUserPoints, (str(sumP), vk_id))
             connect.commit()
+    # В случае неудачи оповещаем об ошибке
     except:
         print("Ошибка в подсчете баллов ЕГЭ")
 
 
 def sumAchPoints(vk_id, connect):
+    """
+    Подсчет баллов за индивидуальные достижения пользователя
+    :param vk_id: идентификатор пользователя в вк
+    :param connect: соединение с БД
+    :return:
+    """
     sumP = 0
     storePoints = 0
+    # Подготавливаем SQL запросы для передачи их базе данных
     bdSelectAch1 = """select * from achievement where id_user = %s and name = %s;"""
     bdSelectAch2 = """select * from achievement where id_user = %s and name = %s;"""
     bdSelectAch3 = """select * from achievement where id_user = %s and name = %s;"""
     bdSelectUser = """select * from user where id = %s;"""
     updateUserPoints = """update user set ege_points = %s where id = %s;"""
+
+    # Передаем запросы на добавление элемента и фиксируем в БД
     try:
         with connect.cursor() as cursor:
             cursor.execute(bdSelectAch1, (str(vk_id),"Золотая медаль"))
@@ -48,32 +65,52 @@ def sumAchPoints(vk_id, connect):
             sumP = sumP + cursor.fetchall()[0]["ege_points"]
             cursor.execute(updateUserPoints, (str(sumP), vk_id))
             connect.commit()
+    # В случае неудачи оповещаем об ошибке
     except:
         print("Ошибка в подсчете баллов за индивидуальные достижения")
 
 
 def deleteAllPoints(vk_id, connect):
+    """
+    Удалить все баллы пользователя
+    :param vk_id: идентификатор пользователя в вк
+    :param connect: соединение с БД
+    :return:
+    """
+    # Подготавливаем SQL запросы для передачи их базе данных
     deleteAch = """delete from achievement where id_user = %s;"""
     deleteEx = """delete from exam where id_user = %s;"""
     updatePoints = """update user set ege_points = 0 where id = %s;"""
 
+    # Передаем запросы на добавление элемента и фиксируем в БД
     try:
         with connect.cursor() as cursor:
             cursor.execute(deleteAch, str(vk_id))
             cursor.execute(deleteEx, str(vk_id))
             cursor.execute(updatePoints, str(vk_id))
             connect.commit()
+    # В случае неудачи оповещаем об ошибке
     except:
         print("Ошибка при удалении баллов")
 
 
 def getAllPoints(vk_id, connect):
+    """
+    Получить все баллы пользователя
+    :param vk_id: идентификатор пользователя в вк
+    :param connect: соединение с БД
+    :return: вернуть все баллы пользователя
+    """
     points = 0
+    # Подготавливаем SQL запрос для передачи его базе данных
     selectDB = """select * from user where id = %s"""
+
+    # Передаем запрос на добавление элемента и фиксируем в БД
     try:
         with connect.cursor() as cursor:
             cursor.execute(selectDB, str(vk_id))
             points = cursor.fetchall()[0]["ege_points"]
+    # В случае неудачи оповещаем об ошибке
     except:
         print("Ошибка при получении баллов ЕГЭ")
     return int(points)
@@ -90,11 +127,19 @@ def getAllPoints(vk_id, connect):
 #     return int(maxPoints)
 
 def choiceSpeciality(vk_id, connect):
+    """
+    Определить по баллам пользователя на какой основе он может обучаться
+    :param vk_id: идентификатор пользователя в вк
+    :param connect: соединение с БД
+    :return: вернуть текст
+    """
+    # Подготавливаем SQL запрос для передачи его базе данных
     selectDB = """select * from speciality"""
     allPoints = getAllPoints(vk_id, connect)
     budgetList = []
     contractList = []
     text = ""
+    # Передаем запрос на добавление элемента и фиксируем в БД
     try:
         with connect.cursor() as cursor:
             cursor.execute(selectDB)
@@ -127,6 +172,6 @@ def choiceSpeciality(vk_id, connect):
                     text = text + "\n" + contractList[i]
 
             return text
-
+    # В случае неудачи оповещаем об ошибке
     except:
         print("Ошибка при получении данных для выбора доступных направлений")
